79311838ea1ccc9e33ac44a31a9be51839175abd
poblouin <pblouin@wealthsimple.com>
2021-12-01 10:27:48 -0500

    Day 01: Part 2

--- 01/solution.js
+++ 01/solution.js
@@ -1,13 +1,32 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("./input", "utf8").split("\n");
+const input = fs.readFileSync("./input", "utf8").split("\n").map(s => Number(s));
 
-const part1 = input.reduce(
-  (accumulator, currentValue, currentIndex, array) =>
-    Number(currentValue) > Number(array[currentIndex - 1])
-      ? accumulator + 1
-      : accumulator,
-  0
-);
+const part1 = () =>
+  input.reduce(
+    (accumulator, currentValue, currentIndex, array) =>
+      currentValue > array[currentIndex - 1]
+        ? accumulator + 1
+        : accumulator,
+    0
+  );
 
-process.stdout.write(`Part 1: ${part1}`);
+const part2 = () => {
+  let count = 0;
+  let delimiter = input[0];
+  let previousWindow = input[0] + input[1] + input[2];
+
+  for (let i = 3; i < input.length; i++) {
+    const currentWindow = previousWindow - delimiter + input[i];
+
+    if (currentWindow > previousWindow) count += 1
+
+    delimiter = input[i - 2];
+    previousWindow = currentWindow;
+  }
+
+  return count;
+}
+
+process.stdout.write(`Part 1: ${part1()}\n`);
+process.stdout.write(`Part 2: ${part2()}`);
