71d9b31a41bb51199d8b45362d8dc99488c44856
poblouin <pblouin@wealthsimple.com>
2021-12-02 10:45:32 -0500

    Day 02: Ruby Solution

---
+++ 02/solution.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+input = File.readlines('input')
+
+def format_input(input, has_aim: false)
+  behaviour = has_aim ? :aim : :depth
+
+  input.map do |element|
+    splitted = element.chomp.split(' ')
+    key = splitted[0] == 'forward' ? :position : behaviour
+    value = splitted[0] == 'up' ? -splitted[1].to_i : splitted[1].to_i
+
+    [key, value]
+  end
+end
+
+def part1(input)
+  format_input(input).each_with_object({ position: 0, depth: 0 }) do |element, accumulator|
+    accumulator[element[0]] += element[1]
+  end
+end
+
+def part2(input)
+  format_input(input, has_aim: true).each_with_object({ position: 0, depth: 0, aim: 0 }) do |element, accumulator|
+    key = element[0]
+    value = element[1]
+
+    accumulator[key] += value
+    accumulator[:depth] += accumulator[:aim] * value if key == :position
+  end
+end
+
+puts part1(input).values.inject(:*)
+puts part2(input).slice(:position, :depth).values.inject(:*)
